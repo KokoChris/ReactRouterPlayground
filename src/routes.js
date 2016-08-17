c948fe4c2c2657652982150e06ca490117b95462
kokoChris <kokolios1986@yahoo.gr>
2016-08-17 16:21:31 +0300

    add axios + redux-promise

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import {Route, IndexRoute} from 'react-router';
 
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
 const Greeting = () => {
 	return <div> Hey there! </div>;
@@ -13,9 +14,8 @@ const Greeting = () => {
 
 export default (
    <Route path='/' component={App} >
-   		<Route path="greet"  component={Greeting} />
-   		<Route path="greet2" component={Greeting} />
-   		<Route path="greet3" component={Greeting} />
+   		<IndexRoute component={PostsIndex} />
+   		
 
 
    </Route>
