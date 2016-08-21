a424569707660a89e7ced7155350b4d65bd19ebe
Chris Kokolios <kokolios1986@yahoo.gr>
2016-08-21 21:59:26 +0300

    finish app

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,6 +11,19 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					<Link to={"posts/"+ post.id}>
+						<span className="pull-xs-right">{post.categories}</span>
+						<strong>{post.title}</strong>
+					</Link>
+				</li>
+		    )
+		})
+	}
+
 	render() {
 		return (
 			<div>
@@ -19,7 +32,11 @@ class PostsIndex extends Component {
 				  		New Post
 				  </Link>	
 				</div> 
-			   List of blog posts
+			 <h3>Posts</h3>
+			 <ul className="list-group">
+			 	{this.renderPosts()}
+
+			 </ul>
 			 </div>
 
 		);
@@ -32,5 +49,9 @@ function mapDispatchToProps(dispatch){
 }
 
 
+function mapStateToProps(state){
+	return{posts:state.posts.all};
+}
 
-export default connect(null,mapDispatchToProps)(PostsIndex);
+
+export default connect(mapStateToProps,mapDispatchToProps)(PostsIndex);
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 const Greeting = () => {
 	return <div> Hey there! </div>;
 }
@@ -15,7 +16,8 @@ const Greeting = () => {
 export default (
    <Route path='/' component={App} >
    		<IndexRoute component={PostsIndex} />
-   		<Route path="/posts/new" component={PostsNew} />
+   		<Route path="posts/new" component={PostsNew} />
+   		<Route path="posts/:id" component={PostsShow} />
 
    </Route>
 )
